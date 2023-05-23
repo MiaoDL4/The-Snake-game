e2e631741ab05fe16d5bdeb40da74967a2307fd1
MLD4 <dongmiao123@gmail.com>
2023-05-23 15:14:58 +0930

    commit before game implementation

--- client/src/pages/snake/solo.js
+++ client/src/pages/snake/solo.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./assets/style.css";
 
+
 const Solo = () => {
+  //grid/play space
+  //snake
+  //pick up randomly generating to the grid but not where the snake is
+  //movement usng arrow keys can not reverse driection
+  //snake grow and moves fast when getting pick up
+  //game over conditons 1 hitting boarder 2 hitting self
+
   return (
     <>
       <div id="game-board"></div>

200a345ec68f0e106c27853652c8f31ce84ded7b
MLD4 <dongmiao123@gmail.com>
2023-06-01 18:44:52 +0930

    heroku fix 3

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 require("dotenv").config();
 
-const secret = process.env;
+const secret = "gotTOTHisthingtowork";
 const expiration = '2h';
 
 module.exports = {
